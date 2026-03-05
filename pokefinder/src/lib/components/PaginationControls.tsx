e632aa87600fe552530a47d22360196d6a9f7f01
JesusGomezJulia <jesusgomezjulia3d@gmail.com>
2026-03-05 01:13:32 +0100

    fix pagination

--- pokefinder/src/lib/components/PaginationControls.tsx
+++ pokefinder/src/lib/components/PaginationControls.tsx
@@ -19,12 +19,12 @@ export function PaginationControls({
 }: PaginationProps) {
   return (
     <Stack direction="row" gap={2} justifyContent="center" alignItems="center">
-      <Button onClick={() => onPageChange(0)} disabled={currentPage === 0}>
+      <Button onClick={() => onPageChange(0)} disabled={currentPage <= 0}>
         <ChevronsLeftIcon size={16} />
       </Button>
       <Button
         onClick={() => onPageChange(currentPage - 1)}
-        disabled={currentPage === 0}
+        disabled={currentPage <= 0}
       >
         <ChevronLeftIcon size={16} />
       </Button>
@@ -33,13 +33,13 @@ export function PaginationControls({
       </Typography>
       <Button
         onClick={() => onPageChange(currentPage + 1)}
-        disabled={currentPage === totalPages - 1}
+        disabled={currentPage >= totalPages - 1}
       >
         <ChevronRightIcon size={16} />
       </Button>
       <Button
         onClick={() => onPageChange(totalPages - 1)}
-        disabled={currentPage === totalPages - 1}
+        disabled={currentPage >= totalPages - 1}
       >
         <ChevronsRightIcon size={16} />
       </Button>
--- pokefinder/src/lib/hooks/usePagination.ts
+++ pokefinder/src/lib/hooks/usePagination.ts
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 export function usePagination<T>(items: T[], itemsPerPage: number) {
   const [page, setPage] = React.useState(0);
-  const totalPages = Math.ceil(items.length / itemsPerPage);
+  const totalPages = Math.max(1, Math.ceil(items.length / itemsPerPage));
   const pagedItems = React.useMemo(
     () => items.slice(page * itemsPerPage, (page + 1) * itemsPerPage),
     [items, page, itemsPerPage],
   );
+
+  useEffect(() => {
+    if (page >= totalPages) {
+      setPage(totalPages - 1);
+    } else if (page < 0) {
+      setPage(0);
+    }
+  }, [page, totalPages]);
+  
   return {
     page,
     setPage,
